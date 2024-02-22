d0df5e2fe7a7a455bd43f260b6f574cb9347a01d
conmart <connor.a.martinelli@gmail.com>
2024-02-22 14:38:10 -0800

    fix bugs add tests

--- test.js
+++ test.js
@@ -1,12 +1,17 @@
 const test = require('node:test');
 const assert = require('assert');
-const { pmToAqi } = require('./utils');
+const { pmToAqi, calcTimeout } = require('./utils');
 
 test('PM to AQI', () => {
+  assert.strictEqual(pmToAqi(-50), 0);
   assert.strictEqual(pmToAqi(6), 25);
   assert.strictEqual(pmToAqi(35), 99);
   assert.strictEqual(pmToAqi(46), 127);
   assert.strictEqual(pmToAqi(105), 177);
   assert.strictEqual(pmToAqi(165), 215);
   assert.strictEqual(pmToAqi(600), 500);
+});
+
+test('Timeout calculation', () => {
+  assert.strictEqual(calcTimeout(0, 6, 1600000), 1600000);
 });
